e15023368e97c3aa38789f55f7876c4c8dc9a751
sushantshukla <48709038+sushantshukla@users.noreply.github.com>
2020-05-13 17:23:43 +0530

    Update 2D arrays(allcodes).cpp

--- 2D arrays(allcodes).cpp
+++ 2D arrays(allcodes).cpp
@@ -100,4 +100,69 @@ void spiralprint(int a[1000][1000],int m, int n){
     spiralprint(a,m,n);
 }
 */
+#include <iostream>
+#include<algorithm>
+using namespace std;
+void rotate(int a[1000][1000],int n){
+    //reerse each row
+    for(int row=0;row<n;row++){
+        int start_col=0;
+        int end_col=n-1;
+        while(start_col<end_col){
+            swap(a[row][start_col],a[row][end_col]);
+            start_col++;
+            end_col--;
+        }
+    }
+    // take the transpose of matrix
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i<j){
+                swap(a[i][j],a[j][i]);
+            }
+        }
+    }
+
+
+}
+/*void rotate_stl(int a[1000][1000], int n){
+    //same thing using stl reverse(start_cont,end_cont) methods
+    for (int i=0;i<n;i++){
+        //a[i]is the row pointer
+        reverse(a[i],a[i]+n);
+    }
+    // take the transpose of matrix
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i<j){
+                swap(a[i][j],a[j][i]);
+            }
+        }
+    }
+
+
+}
+*/
+int main() {
+    //row wise traversal of array
+    int a[1000][1000]={0};// initializing it with all zeros
+    int n;
+    cin>>n;
+    for (int row=0;row<=n-1;row++){
+        for(int col=0;col<=n-1;col++){
+            cin>>a[row][col];
+            
+        }
+        
+    }
+    rotate(a,n);
+    for (int row=0;row<=n-1;row++){
+        for(int col=0;col<=n-1;col++){
+            cout<<a[row][col]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+}
 
