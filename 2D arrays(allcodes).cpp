a14702f4c535a576024ea9e8de4209428374f99a
sushantshukla <48709038+sushantshukla@users.noreply.github.com>
2020-05-13 16:20:19 +0530

    Update 2D arrays(allcodes).cpp

--- 2D arrays(allcodes).cpp
+++ 2D arrays(allcodes).cpp
@@ -46,4 +46,58 @@ int main() {
     }
 return 0;
 }
+//print spiral in 2D matrix
+void spiralprint(int a[1000][1000],int m, int n){
+    int startrow=0;
+    int startcol=0;
+    int endrow=m-1;
+    int endcol=n-1;
+    //print the spiral till (while condition)
+    while (startrow<=endrow and startcol<=endcol){
+        //first row
+        for (int i=startcol;i<=endcol;i++){
+            cout<<a[startrow][i]<<" ";
+        }
+        startrow++;
+        //print last colm
+        for (int i=startrow;i<=endrow;i++){
+            cout<<a[i][endcol]<<" ";
+        }
+        endcol--;
+
+    // print the bottomrow
+        if(endrow>startrow){
+            for (int i=endcol;i>=startcol;i--){
+                cout<<a[endrow][i]<<" ";
+            }
+            endrow--;
+        }
+     //print start colm
+     if(endcol>startcol){
+            for(int i=endrow;i>=startrow;i--){
+                cout<<a[i][startcol]<<" ";
+            }
+            startcol++;
+     }
+
+    }
+}
+
+/*int main() {
+    //row wise traversal of array
+    int a[1000][1000]={0};// initializing it with all zeros
+    int m,n;
+    cin>>m>>n;
+    int val=1;
+    for (int row=0;row<=m-1;row++){
+        for(int col=0;col<=n-1;col++){
+            a[row][col]=val;
+            val++;
+            cout<<a[row][col]<<" ";
+        }
+        cout<<endl;
+    }
+    spiralprint(a,m,n);
+}
+*/
 
