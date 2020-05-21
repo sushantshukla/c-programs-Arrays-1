7a7b7d26c086e337ba090a6579e8b733a506adc5
sushantshukla <48709038+sushantshukla@users.noreply.github.com>
2020-05-21 15:42:04 +0530

    Update 2D arrays(allcodes).cpp

--- 2D arrays(allcodes).cpp
+++ 2D arrays(allcodes).cpp
@@ -303,6 +303,38 @@ int main() {
     }
     return 0;
 }
+#include <iostream>
+#include <cstring>
+using namespace std;
+bool ispallidrome(char a[1000]){
+    int i=0;
+    int j=strlen(a)-1;
+    while(i<j){
+        if(a[i]==a[j]){
+            i++;
+            j--;
+        }
+        else{
+            return false;
+        }
+    }
+    //loop terminates
+    return true;
+}
+int main() {
+    //palindrome check
+    char a[1000];
+   // readline(a,1000);
+    cin.getline(a,1000);
+    if(ispallidrome(a)){
+        cout<<"palindromic string";
+    }
+    else{
+        cout<<"Not a palindrome";
+    }
+    return 0;
+}
+
 
 
 
