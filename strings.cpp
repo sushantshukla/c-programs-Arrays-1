57c49076b207d29d6af75693b4dc59855de558ef
sushantshukla <48709038+sushantshukla@users.noreply.github.com>
2020-05-21 16:38:21 +0530

    Update strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -27,3 +27,40 @@ int main() {
     }
     return 0;
 }
+//remove duplicates
+#include <iostream>
+#include<cstring>
+using namespace std;
+// remove dulpicates character from the string which are consecutive
+//ccoooding---->coding
+void removeDeplicates(char a[]) {
+    int l=strlen(a);
+    if(l==1 or l==0){
+        return ;
+    }
+    //current=i
+    //prev=j// if condition is not 
+    //true still current is getting increased
+    int prev=0;
+    for(int current=1; current<l;current++){
+        if(a[current]!=a[prev]){
+            prev++;
+            a[prev]=a[current];
+        }
+    }
+    a[prev+1]= '\0';
+    return;
+
+
+
+}
+int main() {
+   char a[1000];
+   // readline(a,1000);
+    cin.getline(a,1000);
+    removeDeplicates(a);
+    cout<<a<<endl;
+    
+    return 0;
+}
+
