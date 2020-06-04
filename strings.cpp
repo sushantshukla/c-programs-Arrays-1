96a5a3f41f3afe518c13d076f31810bf39a69521
sushantshukla <sushantshukla92@gmail.com>
2020-06-04 11:56:58 +0530

    Update strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -64,3 +64,33 @@ int main() {
     return 0;
 }
 
+// find largest string of all
+#include <iostream>
+#include<cstring>
+using namespace std;
+int main() {
+    int n;
+    cin>> n;
+    char a[1000];
+    char largest[1000]; 
+
+    int len=0;
+    int largest_len=0;
+    cin.get(); // null character consume
+    for (int i=0;i<n;i++){
+        cin.getline(a,1000);
+        len=strlen(a);
+        if(len > largest_len){
+            largest_len=len;
+            //copy all the characters from array to array largest
+            strcpy(largest ,a);
+        }
+    }
+     cout<< largest<<" and "<< largest_len<<endl;
+    return 0;
+
+
+    
+}
+
+
