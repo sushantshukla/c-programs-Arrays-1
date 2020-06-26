790f0a391d67b8d5268bb326aabe08d9652ace26
sushantshukla <sushantshukla92@gmail.com>
2020-06-26 16:08:15 +0530

    Update strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -204,7 +204,32 @@ int main() {
    
     return 0;
 }
-
+//shorten the string by counting the individual character
+//aaabbccc----------a2b2c3
+#include<iostream>
+int main() { using namespace std;
+char arr[101];
+cin>>arr;   ///CHANGE. Input of strings is taken in this way.
+char ch;
+int count=0;
+ch=arr[0];
+int i=0;
+while(arr[i]!='\0')
+{
+  if(ch==arr[i])
+  {
+    count++;
+  }
+  else{
+    cout<<ch<<count;
+    ch=arr[i];
+    count=1;
+  }
+  i++;
+}
+cout<<ch<<count;
+	return 0;
+}
 
 
 
