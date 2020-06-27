6237253bbf3a303254e8fe9bbdb85700521c18c8
sushantshukla <sushantshukla92@gmail.com>
2020-06-27 17:05:32 +0530

    Update Algorithm_STL.cpp

--- Algorithm_STL.cpp
+++ Algorithm_STL.cpp
@@ -59,3 +59,27 @@ int main() {
 
     return 0;
 }
+//money change problem online video 168 =100,50,10,5,2,1
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool compare(int a , int b){
+    return a<=b;
+}
+
+int main() {
+    int coins[]= {1,2,5,10,20,50,100,200,500,2000};
+    int n=sizeof(coins)/sizeof(int); //will give me the length
+    int money=168;
+    while(money>=0){
+    int lb=lower_bound(coins,coins+n,money,compare)-coins -1;
+    int m=coins[lb];
+    cout<<m<<" ,";
+    money=money-m;
+    
+    }
+    
+   return 0;
+}
+
