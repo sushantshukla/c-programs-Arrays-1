877704d9ee1248f65fff8a1d40fa8a12d806a0bb
sushantshukla <sushantshukla92@gmail.com>
2020-06-27 15:07:54 +0530

    Update Questions_Arrays.cpp

--- Questions_Arrays.cpp
+++ Questions_Arrays.cpp
@@ -128,3 +128,102 @@ int arr[test[i]];
 }
 return 0;
 }
+//form biggest no
+#include<bits/stdc++.h>
+using namespace std;
+bool mycmp(string a,string b){
+    string ab=a.append(b);
+    string ba=b.append(a);
+    return ab.compare(ba)>0;
+}
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+    cin>>n;
+    string s[n];
+    for(int i=0;i<n;i++)
+        cin>>s[i];
+    sort(s,s+n,mycmp);
+    for(int i=0;i<n;i++){
+        cout<<s[i];
+    }
+    cout<<endl;
+    }
+	return 0;
+}
+// 2 arrays sum .
+#include<iostream>
+using namespace std;
+int main()
+{
+	int M[1000],N[1000],A[1000];
+	int sum[1000],carry=0;
+	int m,n;
+	cin>>m;
+	for(int i=0;i<m;i++)
+	{
+		cin>>M[i];
+	}
+	cin>>n;
+	for(int j=0;j<n;j++)
+	{
+		cin>>N[j];
+	}
+    int c[10001],ind=0;
+    int i=n-1,j=m-1;
+    while(i>=0 && j>=0){
+        c[ind]=(N[i]+M[j])%10;ind++;
+        if(N[i]+M[j]>=10){
+            if(i-1>=0){
+                N[i-1]+=1;
+            }
+            else if(j-1>=0){
+                M[j-1]+=1;
+            }
+            else{
+                c[ind]=1;ind++;
+            }
+        }
+        i--;j--;
+    }
+		
+	if(i>=0){
+        while(i>=0){
+            c[ind]=N[i]%10;ind++;
+            if(N[i]>=10){
+                if(i-1>=0){
+                    N[i-1]+=1;
+                }
+                else{
+                    c[ind]=1;ind++;
+                }
+            }
+            i--;
+        }
+    }
+	if(j>=0){
+        while(j>=0){
+            c[ind]=M[j]%10;ind++;
+            if(M[j]>=10){
+                if(j-1>=0){
+                    M[j-1]+=1;
+                }
+                else{
+                    c[ind]=1;ind++;
+                }
+            }
+            j--;
+        }
+    }
+    for(int i=ind-1;i>=0;i--){
+        cout<<c[i]<<", ";
+    }
+    cout<<"END";
+
+	
+return 0;
+	
+	
+}
