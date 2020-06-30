352aa06a869b9645c20bd7a1caef685e70286221
sushantshukla <sushantshukla92@gmail.com>
2020-07-01 00:33:58 +0530

    Update Divide and conquer(sorting_searching).cpp

--- Divide and conquer(sorting_searching).cpp
+++ Divide and conquer(sorting_searching).cpp
@@ -4,33 +4,25 @@ using namespace std;
 
 void bubblesort(int a[],int n)
 {
-	
-	for(int i=0;i<n;i++)
-		{
-			for(int j=0;j<n-i-1;j++)
-				{
-					if(a[j]>a[j+1])
-						{
-							swap(a[j],a[j+1]);
-						}					
-         }
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n-i-1;j++){
+			if(a[j]>a[j+1]){
+				swap(a[j],a[j+1]);
+			}					
 		}
+	}
+	
 	
 }
 
-int main()
-	{
+int main(){
 		
 	int n;
 	cin>>n;
-	
 	int a[n];
-	
-	for(int i=0;i<n;i++)
-		{
+	for(int i=0;i<n;i++){
 			cin>>a[i];
-		}
-
+	}
 	bubblesort(a,n);
 	for(int i=0;i<n;i++)
 			cout<<a[i]<<endl;
