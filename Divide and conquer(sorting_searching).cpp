7a1f404efda02cd2d2a9f0a82e471ac7a3c69583
sushantshukla <sushantshukla92@gmail.com>
2020-07-03 23:20:43 +0530

    Update Divide and conquer(sorting_searching).cpp

--- Divide and conquer(sorting_searching).cpp
+++ Divide and conquer(sorting_searching).cpp
@@ -152,5 +152,52 @@ int main()
    cout<<find_key(a, n, key)<<endl;
    return 0;
 }
+//square root using Binary Search
+#include <iostream>
+using namespace std;
+float square_root(int n,int p){
+    int s=0;
+    int e=n;
+    float ans=-1;
+    while(s<=e){
+        int mid=(s+e) ;
+        if(mid*mid==n){
+            return mid;
+        }
+        if(mid*mid<n){
+            ans=mid;
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+    }
+    //foating part
+    //brute force 
+    float inc=0.1;
+    for (int times=1; times<=p;times++){
+        while(ans*ans<=n){
+            ans=ans +inc;
+        }
+        //overshoot or one step backward
+        ans=ans-inc;
+        inc=inc/10;
+    }
+    return ans;
+
+}
+
+
+
+int main() {
+    int n;
+    cin>>n;
+    int p=5;
+    cout<<square_root(n,p)<<endl;
+
+    
+    return 0;
+}
+
 
 
