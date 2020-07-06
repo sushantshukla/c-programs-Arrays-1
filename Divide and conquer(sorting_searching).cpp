eb233006cb1aae1940082075ed7657330381e82a
sushantshukla <sushantshukla92@gmail.com>
2020-07-07 00:53:14 +0530

    Update Divide and conquer(sorting_searching).cpp

--- Divide and conquer(sorting_searching).cpp
+++ Divide and conquer(sorting_searching).cpp
@@ -303,5 +303,75 @@ int main() {
 	return 0;
 }
 
+//book allocation problem
+#include<iostream>
+#include<algorithm>
+#include<climits>
+using namespace std;
+bool ispossible(int arr[], int n, int m, int curr_min){
+    int studentused=1;
+    int pages_reading= 0;
+    for(int i=0;i<n;i++){
+        if(pages_reading+arr[i]> curr_min){
+            studentused++;
+            pages_reading=arr[i];
+            if(studentused>m){
+                return false;
+            }
+        }
+        else {
+            pages_reading +=arr[i];
+        }
+
+    }
+    return true;
+}
+
+int bookallocation(int n , int m, int arr[]){
+    long long sum=0;
+	
+    if (n<m){
+        return -1;
+    }
+    //count the no of pages
+    for (int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    int s=arr[n-1];
+	int e=sum;
+    int ans=INT_MAX;
+    while(s<=e){
+        int mid=(s+e)/2 ;
+        if (ispossible(arr,n,m,mid)){
+            ans= min(ans,mid);
+            e=mid-1;
+        }
+        else{
+            //when not possin=ble to divide in x pages
+            s=mid+1;
+        }
+    }
+    return ans;
+	
+
+
+}
+int main() {
+	int t,m,n;
+	int arr[1000];
+	cin>>t;
+	for (int i=0;i<t;i++){
+		cin>> n >> m;
+		for(int j=0;j<n;j++){
+			cin>>arr[j];
+
+		}
+
+        cout<<bookallocation(n,m,arr)<<endl;
+	}
+	
+	return 0;
+}
+
 
 
